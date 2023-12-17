2268356820c24de8abbf14373ce82d9d6cad77bf
Daniel Padrino <danswarrior1@gmail.com>
2023-12-17 13:44:25 -0300

    Add @nestjs/config dependency and configure global module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PriceModule } from './price/price.module';
+import { AddressesModule } from './addresses/addresses.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PriceModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PriceModule,
+    AddressesModule,
+  ],
 })
 export class AppModule {}
